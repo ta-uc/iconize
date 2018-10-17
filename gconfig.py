6bbcdf5ba1123ff281a2faa37a6a437552ae86a1
Uchida Takumi <mitauchiku@gmail.com>
2018-10-17 18:15:23 +0900

    Mod gunicorn config

--- gconfig.py
+++ gconfig.py
@@ -2,10 +2,11 @@ import multiprocessing
 import os
 mem_mb = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES') / 1024 **2
 w = multiprocessing.cpu_count() * 2 + 1
-need_mem = w * 50
+each_mem = 70
+need_mem = w * each_mem
 while(need_mem > mem_mb):
     w = w -1
-    need_mem = w * 45
+    need_mem = w * each_mem
 workers = w
 secure_scheme_headers = {'X-FORWARDED-PROTOCOL': 'ssl', 'X-FORWARDED-PROTO': 'https', 'X-FORWARDED-SSL': 'on'}
 timeout = 300
